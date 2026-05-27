1d853a49222984599b3daff62b360d7ea7020f8c
Júlio Silvestre <juliosilvestr3@gmail.com>
2026-05-27 01:17:59 -0300

    Salvamento dos arquivos html com a tag Meta

--- js/database.js
+++ js/database.js
@@ -1,3 +1,19 @@
+// Interface/Classe do Subject (Quem é observado)
+class DatabaseSubject {
+    constructor() {
+        this.observers = [];
+    }
+    subscribe(observer) {
+        this.observers.push(observer);
+    }
+    unsubscribe(observer) {
+        this.observers = this.observers.filter(obs => obs !== observer);
+    }
+    notify(event, data) {
+        this.observers.forEach(observer => observer.update(event, data));
+    }
+}
+
 class DatabaseConnection {
     constructor() {
         if (DatabaseConnection.instance) {
